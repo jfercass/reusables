a7d7c6d97a45e1a19e94efede542e12bcff5f836
jfercass <jfercass@gmail.com>
2019-05-14 00:23:19 -0300

    api rest init

---
+++ ApiRest/src/main/java/com/reusable/JpaApplication.java
@@ -0,0 +1,17 @@
+package com.reusable;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+@ServletComponentScan(basePackages = "com.reusable.web")
+@SpringBootApplication(scanBasePackages = {"com.mahindracomviva.web", "com.reusable.rest", "com.reusable.domain"})
+public class JpaApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+}
